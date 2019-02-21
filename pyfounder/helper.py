70c23508696ee1b4f84b5baeb121694150b65c18
Ralph Thesen <mail@redimp.de>
2019-02-21 17:46:23 +0100

    /fetch done

--- pyfounder/helper.py
+++ pyfounder/helper.py
@@ -3,6 +3,7 @@
 # vim: set et ts=8 sts=4 sw=4 ai fenc=utf-8:
 
 import os
+import yaml
 
 class ConfigException(Exception):
     pass
@@ -23,18 +24,46 @@ def get_hosts_yaml():
     p = app.config['PYFOUNDER_HOSTS']
     if not len(p)>0:
         raise ConfigException("Not configured.".format(p))
-    if not os.path.isfile(d):
+    if not os.path.isfile(p):
         raise ConfigException("File {} not found.".format(p))
     if not os.access(p, os.R_OK):
         raise ConfigException("File {} is not readable.".format(p))
+    return p
 
 def get_template_directory():
     from pyfounder import app
-    p = app.config['PXECFG_DIRECTORY']
+    p = app.config['PYFOUNDER_TEMPLATES']
     if not len(p)>0:
         raise ConfigException("Not configured.".format(p))
     if not os.path.isdir(p):
         raise ConfigException("Directory {} not found.".format(p))
-    if not os.access(p, os.W_OK):
-        raise ConfigException("Directory {} is not writeable.".format(p))
     return p
+
+def load_hosts_yaml(filename=None):
+    if filename is None:
+        filename = get_hosts_yaml()
+    try:
+        with open(filename, 'r') as f:
+            return yaml.load(f)
+    except (yaml.MarkedYAMLError) as e:
+        raise ConfigException("Unable to load {}: {}.".format(filename,e))
+    except (IOError, yaml.YAMLError) as e:
+        raise ConfigException("Unable to load {}: {}.".format(filename,e))
+
+def load_hosts_config(filename=None):
+    d = load_hosts_yaml(filename)
+    hosts = {}
+    for hostname, cfg in d['hosts'].items():
+        if 'class' in cfg:
+            if cfg['class'] in d['classes']:
+                hosts[hostname] = d['classes'][cfg['class']]
+        for key,value in cfg.items():
+            hosts[hostname][key] = value
+        # that looks strange, but it necessary to get everything
+        # in one place, when generating the templates
+        hosts[hostname]['hostname'] = hostname
+    return hosts
+
+def host_config(hostname):
+    hosts = load_hosts_config()
+    return hosts[hostname]
--- pyfounder/views.py
+++ pyfounder/views.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 # vim: set et ts=8 sts=4 sw=4 ai fenc=utf-8:
 
-from flask import render_template
+from pprint import pformat
+
+from flask import render_template, Response, abort
 
 from pyfounder import app
 from pyfounder import helper
@@ -30,6 +32,47 @@ def config():
     except helper.ConfigException as e:
         settings['PYFOUNDER_TEMPLATES'] = (None, "{}".format(e))
 
+    try:
+        extra = pformat(helper.load_hosts_config())
+    except helper.ConfigException as e:
+        extra = "Error: {}".format(e)
     # add settings to the config_arr
-    return render_template('config.html', settings=settings)
+    return render_template('config.html', settings=settings, extra=extra)
 
+@app.route('/fetch/<string:hostname>')
+@app.route('/fetch/<string:hostname>/')
+@app.route('/fetch/<string:hostname>/<string:template_name>')
+def fetch(hostname,template_name=None):
+    try:
+        cfg = helper.host_config(hostname)
+    except ValueError as e:
+        abort(404, "Hostname {} not found.".format(hostname))
+    if template_name is None:
+        from yaml import dump
+        ymlcfg = dump(cfg)
+        return Response(ymlcfg, mimetype='text/plain')
+    # find template filename
+    try:
+        template_file= cfg['templates'][template_name]
+    except KeyError as e:
+        abort(404, "Template {} not configured for host {}.".format(
+            template_name,hostname))
+    # load the jinja stuff
+    from jinja2 import FileSystemLoader, Environment
+    from jinja2.exceptions import TemplateNotFound
+    # create context
+    context = cfg
+    for key, value in cfg['variables'].items():
+        context[key] = value
+    # FIXME: should env be global?
+    env = Environment(
+            loader=FileSystemLoader(helper.get_template_directory()))
+    try:
+        # load template
+        template = env.get_template(template_file)
+    except TemplateNotFound as e:
+        abort(404, "Template {} not found.".format(template_file))
+    # render
+    rendered_content = template.render(**context)
+    return Response(rendered_content, mimetype='text/plain')
+    abort(400)
