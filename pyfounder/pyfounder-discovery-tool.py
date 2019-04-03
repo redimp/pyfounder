a227586368205ffa242c67346dbae84e1dc5de11
Ralph Thesen <ralph.thesen@scai.fhg.de>
2019-04-03 16:32:37 +0200

    added lsblk, sfdisk, lspci

--- pyfounder/pyfounder-discovery-tool.py
+++ pyfounder/pyfounder-discovery-tool.py
@@ -6,6 +6,24 @@ import os
 import sys
 import re
 
+from subprocess import getstatusoutput
+
+import json
+from yaml import load, dump, add_representer
+try:
+        from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+        from yaml import Loader, Dumper
+
+Dumper.org_represent_str = Dumper.represent_str
+
+def repr_str(dumper, data):
+    if '\n' in data:
+        return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='|')
+    return dumper.org_represent_str(data)
+
+add_representer(str, repr_str, Dumper=Dumper)
+
 def cpu_model():
     try:
         with open('/proc/cpuinfo') as f:
@@ -24,7 +42,7 @@ def ram_kbytes():
     except:
         return '?'
 
-def getmac(interface):
+def get_mac(interface):
     try:
         mac = open('/sys/class/net/'+interface+'/address').readline()
     except:
@@ -33,10 +51,52 @@ def getmac(interface):
 
 def get_primary_network_interface():
     # ip route list | grep default | ...
-    return None
+    status, output = getstatusoutput("ip route list | grep default")
+    if status > 0:
+        return '?'
+    interface = re.findall("dev\s+([a-zA-Z0-9]+)", output)[0]
+    return interface
 
 def ram_bytes():
     return ram_kbytes() * 1024
 
-print("cpu_model = {}".format(cpu_model()))
-print("ram_bytes = {}".format(ram_bytes()))
+def lsblk():
+    status, output = getstatusoutput("lsblk --raw -n --output NAME,SIZE,TYPE | grep 'disk$'")
+    # parse output
+    data = {}
+    try:
+        for line in output.splitlines():
+            col = line.split()
+            data[col[0]]=col[1]
+    except:
+        return None
+
+    return data
+
+def sfdisk(disk):
+    status, output = getstatusoutput("sfdisk --dump /dev/{}".format(disk))
+    if status != 0:
+        return '?'
+    return output
+
+def lspci():
+    status, output = getstatusoutput("lspci")
+    if status != 0:
+        return '?'
+    return output
+
+
+data = {}
+data["cpu_model"] = cpu_model()
+data["ram_bytes"] = ram_bytes()
+data["mac"]       = get_mac(get_primary_network_interface())
+data["lspci"]     = lspci()
+# disk info
+_lsblk = lsblk()
+data["lsblk"]     = _lsblk or '?'
+# sfdisk partition info
+data['sfdisk']    = {}
+if _lsblk is not None:
+    for disk in _lsblk.keys():
+        data['sfdisk'][disk] = sfdisk(disk)
+print(dump(data, Dumper=Dumper))
