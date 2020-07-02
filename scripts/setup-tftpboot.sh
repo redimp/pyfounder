99abc8299c3c28677dcfc5cfd5f20f87791ecb01
Ralph Thesen <ralph.thesen@scai.fhg.de>
2020-07-02 15:47:40 +0200

    adding uefi boot via grub

--- scripts/setup-tftpboot.sh
+++ scripts/setup-tftpboot.sh
@@ -23,10 +23,18 @@ NETBOOT_TAR_GZ_URL=http://archive.ubuntu.com/ubuntu/dists/${RELEASE}-updates/mai
 
 NETBOOT_TAR_GZ=${WORK_DIR}/download/${RELEASE}-netboot.tar.gz
 
+GRUB2_URL=http://archive.ubuntu.com/ubuntu/dists/${RELEASE}/main/uefi/grub2-amd64/current/grubnetx64.efi
+
+GRUB2_IMAGE=${WORK_DIR}/download/grubnetx64.efi
+
 mkdir -p ${WORK_DIR}/download
 
 if [ ! -f $NETBOOT_TAR_GZ ]; then
 	wget -O $NETBOOT_TAR_GZ $NETBOOT_TAR_GZ_URL
+fi
+
+if [ ! -f $GRUB2_IMAGE ]; then
+	wget -O $GRUB2_IMAGE $GRUB2_URL
 fi
 
 mkdir -p $TARGET_DIR/${RELEASE}
@@ -64,6 +72,51 @@ fi
 
 if [ ! -f $TARGET_DIR/pxelinux.cfg/default ]; then
 	cp $TARGET_DIR/pxelinux.cfg/${RELEASE}-installer $TARGET_DIR/pxelinux.cfg/default
+fi
+
+# UEFI / GRUB2
+mkdir -p $TARGET_DIR/grub/x86_64-efi
+
+if [ ! -f $TARGET_DIR/grub/x86_64-efi/grubnetx64.efi ]; then
+	cp $GRUB2_IMAGE $TARGET_DIR/grub/x86_64-efi/grubnetx64.efi
+fi
+
+if [ ! -e $TARGET_DIR/grubx64.efi ]; then
+	(
+	cd $TARGET_DIR
+	ln -s grub/x86_64-efi/grubnetx64.efi grubx64.efi
+	)
+fi
+
+if [ ! -f ${WORK_DIR}/download/grub-pc-bin*amd64.deb ]; then
+	cd ${WORK_DIR}/download
+	apt download grub-pc-bin
+fi
+
+if [ ! -f ${WORK_DIR}/download/grub-pc-bin/usr/lib/grub/i386-pc/regexp.mod ]; then
+	mkdir -p ${WORK_DIR}/download/grub-pc-bin
+	D=$(ls -1 ${WORK_DIR}/download/grub-pc-bin*amd64.deb | tail -1)
+	dpkg -x $D ${WORK_DIR}/download/grub-pc-bin
+fi
+
+if [ ! -f $TARGET_DIR/grub/x86_64-efi/regexp.mod ]; then
+	cp ${WORK_DIR}/download/grub-pc-bin/usr/lib/grub/i386-pc/regexp.mod $TARGET_DIR/grub/x86_64-efi/regexp.mod
+fi
+
+if [ ! -f $TARGET_DIR/grub/grub.cfg ]; then
+	cat << EOF >$TARGET_DIR/grub/grub.cfg
+# pyfounder example grub.cfg
+
+insmod regexp
+regexp --set=1:m1 --set=2:m2 --set=3:m3 --set=4:m4 --set=5:m5 --set=6:m6 '^([0-9a-f]{1,2})\:([0-9a-f]{1,2})\:([0-9a-f]{1,2})\:([0-9a-f]{1,2})\:([0-9a-f]{1,2})\:([0-9a-f]{1,2})' "\$net_default_mac"
+mac=\${m1}-\${m2}-\${m3}-\${m4}-\${m5}-\${m6}
+
+menuentry '${RELEASE} installer' -id ${RELEASE}_installer {
+    linux ${RELEASE}/ubuntu-installer/amd64/linux vga=normal initrd=${RELEASE}/ubuntu-installer/amd64/initrd.gz locale=en_US.UTF-8 debian-installer/keymap=us pkgsel/install-language-support=false netcfg/dhcp_timeout=120 console-setup/layoutcode=us console-setup/ask_detect=false
+    initrd ${RELEASE}/ubuntu-installer/amd64/initrd.gz
+}
+EOF
+
 fi
 
 echo setup-tftpboot ... done.
